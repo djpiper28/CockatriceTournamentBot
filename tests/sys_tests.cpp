7550374dfe676bb954faba2da5ca4955b8abea2f
Danny Piper <djpiper28@gmail.com>
2021-12-26 21:05:55 +0000

    test endgame fail condition

--- tests/sys_tests.cpp
+++ tests/sys_tests.cpp
@@ -37,7 +37,7 @@ size_t write_callback(char *ptr_in,
                       << std::endl;
             return 0;
         }
-        
+
         response->ptr[size * nmemb] = '\0';
         response->len = size * nmemb;
         memcpy(response->ptr, ptr_in, size * nmemb);
@@ -50,16 +50,16 @@ size_t write_callback(char *ptr_in,
                       << std::endl;
             return 0;
         }
-        
+
         response->ptr[response->len + (size * nmemb)] = '\0';
         memcpy(response->ptr + response->len, ptr_in, size * nmemb);
         response->len += size * nmemb;
     }
-    
+
 #ifdef DEBUG
     std::cerr << "Debug write_callback() : " << response->ptr << std::endl;
 #endif
-    
+
     return size * nmemb;
 }
 
@@ -72,24 +72,24 @@ void freeCurlResponse(struct CurlResponse *resp)
 }
 
 std::string sendRequest(std::string url,
-								        std::string data)
+                        std::string data)
 {
 #ifdef DEBUG
     std::cerr << "Debug sendRequest() : The URL is " << url
               << std::endl;
 #endif
-    
+
     CURL *curl = curl_easy_init();
     if(curl) {
         CURLcode res;
-        
+
         struct CurlResponse response = {NULL, 0};
-        
+
         // Set timeouts
         curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
         curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 2L);
-        
+
         // Set url, user-agent and, headers
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_USE_SSL, 1L);
@@ -99,27 +99,27 @@ std::string sendRequest(std::string url,
         curl_easy_setopt(curl, CURLOPT_HTTPPOST, 1L);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
         curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, data.size());
-        
+
         // Set response write
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &write_callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &response);
-        
+
         res = curl_easy_perform(curl);
 
         bool getSuccess = res == CURLE_OK && response.ptr != NULL;
-        
+
         std::string resp;
         if (getSuccess) {
             resp = std::string(response.ptr);
         }
-        
+
         curl_easy_cleanup(curl);
         freeCurlResponse(&response);
-        
+
         if (!getSuccess) {
             if (res != CURLE_OK) {
                 std::cerr << "Error sendRequest() : curl perform "
-                             "failed after send."
+                          "failed after send."
                           << std::endl;
                 fprintf(stderr,
                         "curl_easy_perform() failed: %s\n",
@@ -130,7 +130,7 @@ std::string sendRequest(std::string url,
             }
             throw NET_ERROR;
         }
-        
+
         return resp;
     } else {
         std::cerr << "Error sendRequest() : curl init failed."
@@ -144,18 +144,18 @@ std::string getRequest(std::string url)
 #ifdef DEBUG
     std::cerr << "Debug getRequest() : The URL is " << url<< std::endl;
 #endif
-    
+
     CURL *curl = curl_easy_init();
     if(curl) {
         CURLcode res;
-        
+
         struct CurlResponse response = {NULL, 0};
-        
+
         // Set timeouts
         curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
         curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 2L);
-        
+
         // Set url, user-agent and, headers
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_USE_SSL, 1L);
@@ -163,23 +163,23 @@ std::string getRequest(std::string url)
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
         curl_easy_setopt(curl, CURLOPT_USERAGENT, "cockatrice-tournament-bot");
         curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
-        
+
         // Set response write
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &write_callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &response);
-        
+
         res = curl_easy_perform(curl);
-        
+
         bool getSuccess = res == CURLE_OK && response.ptr != NULL;
-        
+
         std::string resp;
         if (getSuccess) {
             resp = std::string(response.ptr);
         }
-        
+
         curl_easy_cleanup(curl);
         freeCurlResponse(&response);
-        
+
         if (!getSuccess) {
             if (res != CURLE_OK) {
                 std::cerr << "Error getRequest() : curl perform failed."
@@ -187,67 +187,72 @@ std::string getRequest(std::string url)
                 fprintf(stderr, "curl_easy_perform() failed: %s\n",
                         curl_easy_strerror(res));
             } else {
-                std::cerr << "Error getRequest() : no response was read." 
+                std::cerr << "Error getRequest() : no response was read."
                           << std::endl;
             }
             throw NET_ERROR;
         }
-        
+
         return resp;
     } else {
-        std::cerr << "Error getRequest() : curl init failed." 
+        std::cerr << "Error getRequest() : curl init failed."
                   << std::endl;
         throw NET_ERROR;
     }
 }
 
-void fail(char *reason) {
-   fprintf(stderr, "Fail: %s\n", reason);
-   exit(1);
+void fail(char *reason)
+{
+    fprintf(stderr, "Fail: %s\n", reason);
+    exit(1);
 }
 
 struct args_struct {
-	  int argc;
-	  char **argv;
+    int argc;
+    char **argv;
 };
 
-void *start_src_main(void *args_in) {
-	  struct args_struct *args = (struct args_struct *) args_in;
-	  src_main(args->argc, args->argv);
-	  pthread_exit(NULL);
+void *start_src_main(void *args_in)
+{
+    struct args_struct *args = (struct args_struct *) args_in;
+    src_main(args->argc, args->argv);
+    pthread_exit(NULL);
 }
 
-std::list<std::string> get_lines(std::string text) {
-	  std::list<std::string> ret;
-		std::string line;
+std::list<std::string> get_lines(std::string text)
+{
+    std::list<std::string> ret;
+    std::string line;
     std::stringstream streamData(text);
     while (std::getline(streamData, line, '\n')) {
         ret.push_back(line);
     }
-		
-	  return ret;
-}
 
-std::string get_tag(std::string in) {
-    std::string ret = in;
-    size_t index = ret.find("=");
-    if (index == std::string::npos) return ""; // Failed to find
-    
-    ret = ret.substr(0L, index);
-    printf("INFO-tag: %s\n", ret.c_str());
-     
     return ret;
 }
 
-std::string get_prop(std::string in) {
+std::string get_tag(std::string in)
+{
+    std::string ret = in;
+    size_t index = ret.find("=");
+    if (index == std::string::npos) return ""; // Failed to find
+
+    ret = ret.substr(0L, index);
+    printf("INFO-tag: %s\n", ret.c_str());
+
+    return ret;
+}
+
+std::string get_prop(std::string in)
+{
     std::string ret = in;
     size_t index = ret.find("=");
     if (index == std::string::npos) return ""; // Failed to find
 
     ret = ret.substr(index + 1L);
     printf("INFO-prop: %s\n", ret.c_str());
-				
-		return ret;
+
+    return ret;
 }
 
 #define STR(x) std::string(x)
@@ -266,97 +271,113 @@ std::string get_prop(std::string in) {
 
 struct game_info {
     int gameid;
-		std::string replayname;
+    std::string replayname;
 };
 
-struct game_info test_create_game(struct Config config, int pdi) {
-		std::string api_request = STR("authtoken=") + config.authToken;
-		api_request += endl;
-		api_request += STR("gamename=") + GAME_NAME;
-		api_request += endl;
-		api_request += STR("password=") + PASSWORD;
-		api_request += endl;
-		api_request += STR("playerCount=2");
-		api_request += endl;
-		api_request += STR("spectatorsAllowed=1");
-		api_request += endl;
-		api_request += STR("spectatorsNeedPassword=1");
-		api_request += endl;
+struct game_info test_create_game(struct Config config, int pdi)
+{
+    std::string api_request = STR("authtoken=") + config.authToken;
+    api_request += endl;
+    api_request += STR("gamename=") + GAME_NAME;
+    api_request += endl;
+    api_request += STR("password=") + PASSWORD;
+    api_request += endl;
+    api_request += STR("playerCount=2");
+    api_request += endl;
+    api_request += STR("spectatorsAllowed=1");
+    api_request += endl;
+    api_request += STR("spectatorsNeedPassword=1");
+    api_request += endl;
     api_request += STR("spectatorsCanChat=1");
-		api_request += endl;
+    api_request += endl;
     api_request += STR("spectatorsCanSeeHands=1");
-		api_request += endl;
+    api_request += endl;
     api_request += STR("onlyRegistered=1");
-		api_request += endl;
+    api_request += endl;
 
     // Player deck verification
     if (pdi) {
         api_request += STR("playerDeckVerification=1");
-    		api_request += endl;
+        api_request += endl;
         api_request += STR("playerName=") + P1_NAME;
-    		api_request += endl;
+        api_request += endl;
         api_request += STR("deckHash=") + P1_DECK_1;
-    		api_request += endl;
+        api_request += endl;
 
         api_request += STR("playerName=") + P2_NAME;
-    		api_request += endl;
+        api_request += endl;
         api_request += STR("deckHash=") + P2_DECK_1;
-    		api_request += endl;
+        api_request += endl;
         api_request += STR("deckHash=") + P2_DECK_2;
-		    api_request += endl;
-		}
+        api_request += endl;
+    }
 
     // Make request
     int gameid = -1;
     std::string replayname = "";
-    std::string base_url = STR(config.bindAddr); 
-		std::string ret = sendRequest(base_url + STR("/api/creategame"), api_request);
-		if (ret == "timeout error") fail("timeout error for game create");
-		if (ret == "invalid player info") fail("invalid player info for game create");
-	  		
-	  // Get data from api call
-		std::string line;
-		std::list<std::string> lines = get_lines(ret);
+    std::string base_url = STR(config.bindAddr);
+    std::string ret = sendRequest(base_url + STR("/api/creategame"), api_request);
+    if (ret == "timeout error") fail("timeout error for game create");
+    if (ret == "invalid player info") fail("invalid player info for game create");
+
+    // Get data from api call
+    std::string line;
+    std::list<std::string> lines = get_lines(ret);
     while (lines.size() > 0) {
-    	  line = lines.back();
-    	  lines.pop_back();
-				std::string tag = get_tag(line);
-				std::string prop = get_prop(line);
+        line = lines.back();
+        lines.pop_back();
+        std::string tag = get_tag(line);
+        std::string prop = get_prop(line);
 
-				// Guard against invalid or empty lines
-				if (tag == "" || prop == "") continue;
+        // Guard against invalid or empty lines
+        if (tag == "" || prop == "") continue;
 
-				if (tag == "gameid") { gameid = atoi(prop.c_str()); }
-				if (tag == "replayName") { replayname = prop; }
-		}
-		
-	  // Check if valid
-		if (gameid < 1) fail("no gameid for game create"); // also checks for atoi fails
-		if (replayname == "") fail("no replay name for game create");
-		
-	  struct game_info info = {gameid, replayname};
+        if (tag == "gameid") {
+            gameid = atoi(prop.c_str());
+        }
+        if (tag == "replayName") {
+            replayname = prop;
+        }
+    }
+
+    // Check if valid
+    if (gameid < 1) fail("no gameid for game create"); // also checks for atoi fails
+    if (replayname == "") fail("no replay name for game create");
+
+    struct game_info info = {gameid, replayname};
     return info;
 }
 
-void test_end_game(struct Config config, struct game_info info) {
+void test_end_game(struct Config config, struct game_info info, int expect_error)
+{
     std::string request = STR("authtoken=") + config.authToken + endl;
     request += STR("gameid=") + std::to_string(info.gameid) + endl;
 
-		std::string ret = sendRequest(config.bindAddr + STR("/api/endgame"), request);
-		if (ret != "success") fail("end game failed");
+    std::string ret = sendRequest(config.bindAddr + STR("/api/endgame"), request);
+    if (expect_error) {
+        if (ret == "success") fail("end game failed, error expected");
+    } else {
+        if (ret != "success") fail("end game failed");
+    }
 }
 
-int test_api(struct Config config) {
+int test_api(struct Config config)
+{
     struct game_info info_no_pdi = test_create_game(config, 0);
-		
-		printf("Sleeping 1.\n");
+
+    printf("Sleeping 1.\n");
     sleep(1);
-		std::string base_url = STR(config.bindAddr);
+    std::string base_url = STR(config.bindAddr);
     std::string get_status_game = getRequest(base_url + STR("/") + STR(info_no_pdi.replayname));
-    if (get_status_game.find(GAME_NAME) == std::string::npos) 
+    if (get_status_game.find(GAME_NAME) == std::string::npos)
         fail("Failed to get game status for game with no pdi");
 
-    test_end_game(config, info_no_pdi);
+    test_end_game(config, info_no_pdi, 0);
+
+    // Test not found
+    struct game_info info_test = info_no_pdi;
+    info_test.gameid = 180;
+    test_end_game(config, info_test, 1);
 
     struct game_info info_pdi = test_create_game(config, 1);
 
@@ -364,41 +385,42 @@ int test_api(struct Config config) {
     get_status_game = getRequest(base_url + STR("/") + STR(info_pdi.replayname));
     if (get_status_game.find(GAME_NAME) == std::string::npos)
         fail("Failed to get game status for game with pdi");
-    
+
     if (get_status_game.find(P1_NAME) == std::string::npos) fail("p1 name not found");
     if (get_status_game.find(P1_DECK_1) == std::string::npos) fail("p1 deck not found");
-    
+
     if (get_status_game.find(P2_NAME) == std::string::npos) fail("p2 name not found");
     if (get_status_game.find(P2_DECK_1) == std::string::npos) fail("p2 hash 1 not found");
     if (get_status_game.find(P2_DECK_2) == std::string::npos) fail("p2 hash 2 not found");
-    
-    test_end_game(config, info_pdi);
-    
-	  return 0;
+
+    test_end_game(config, info_pdi, 0);
+
+    return 0;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     struct args_struct args_in = {argc, argv};
-	  pthread_t main_thread;	  
-	  pthread_create(&main_thread, NULL, start_src_main, (void *) &args_in);
+    pthread_t main_thread;
+    pthread_create(&main_thread, NULL, start_src_main, (void *) &args_in);
 
-		// Wait for startup to complete
-		fprintf(stderr, "Waiting %d to start system tests\n", WAIT);
-	  for (int i = 0; i < WAIT; i++) {
-	  	  fprintf(stderr, "%d...\n", WAIT - i);
-	  	  sleep(1);
-	  }
+    // Wait for startup to complete
+    fprintf(stderr, "Waiting %d to start system tests\n", WAIT);
+    for (int i = 0; i < WAIT; i++) {
+        fprintf(stderr, "%d...\n", WAIT - i);
+        sleep(1);
+    }
 
-	  fprintf(stderr, "Tests have started\n.");
-	  int status = 0;
+    fprintf(stderr, "Tests have started\n.");
+    int status = 0;
 
-		struct Config config;
+    struct Config config;
     status |= !readConf(&config, "config.conf");
     if (status) fail("Cannot read config.conf");
     if (config.bindAddr == NULL) fail("NULL bind address");
 
     std::string base_url = STR(config.bindAddr);
-		
+
     // Test api page
     status = ERROR == getRequest(base_url + "/api");
     if (status) fail("Cannot get /api");
@@ -442,15 +464,15 @@ int main(int argc, char **argv) {
     status = ERROR == getRequest(base_url + "/api/version/");
     if (status) fail("Cannot get /api/version/");
 
-		std::string token = std::string(config.authToken);
+    std::string token = std::string(config.authToken);
 
     // Check auth key
     status = "valid=1" != sendRequest(base_url + "/api/checkauthkey", token);
     if (status) fail("Cannot checkauthkey");
 
     status = test_api(config);
-		
-		// Do not free anything as I am lazy
-	  return status;
+
+    // Do not free anything as I am lazy
+    return status;
 }
 
